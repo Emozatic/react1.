74898dafde6ba1b5d31c83847bdcf4569a496f0b
Lucky Sah <lk4179869@gmail.com>
2026-05-22 22:45:01 +0530

    add: dynamic component styling

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -5,9 +5,8 @@ function App(){
   return (
     <>
     <h1>Hello World!</h1>
-    <ConditionPrice title="phone" price={10000} />
+    <ConditionPrice title="phone" price={10000} style={{ color: 'blue' }} />
     </>
   )
 }
-\j
 export default App;
--- day2/src/ConditionPrice.jsx
+++ day2/src/ConditionPrice.jsx
@@ -1,9 +1,9 @@
-export default function ConditionPrice({title, price }){
+export default function ConditionPrice({title, price, style}){
     return(
         <>
         <h1>{title}</h1>
         <p>{price}</p>
-        <p>{price>5000 ? "5% discount" : null}</p>
+        <p>{price>5000 ? <h1 style={style}>5% discount</h1> : null}</p>
         </>
     )
 }

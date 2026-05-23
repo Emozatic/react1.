bfe59a94bffd0a3fab8ca2e47ae484d59b8218bc
Lucky Sah <lk4179869@gmail.com>
2026-05-23 23:31:16 +0530

    add: apply useState for many values

--- day3/src/ButtonClick.jsx
+++ day3/src/ButtonClick.jsx
@@ -4,16 +4,31 @@ function buttonClick(){
     alert("Button Clicked!");
 }
 
-export default function ButtonClick({title}){
-    let [count, setCount] = useState(0);
-    let countClick=()=>{
-        setCount(count+1);
-        console.log(count);
-    }
+// export default function ButtonClick({title}){
+//     let [count, setCount] = useState(0);
+//     let countClick=()=>{
+//         setCount(count+1);
+//         console.log(count);
+//     }
+//     return(
+//         <>
+//         <p>Count: {count}</p>
+//         <button onClick={countClick}>{title}</button>
+//         </>
+//     )
+// }
+
+//storing names
+ 
+export default function ButtonClick(){
+    const names= ["Alice", "Bob", "Charlie", "David", "Eve"];
+    const [index, setName]= useState(0);
     return(
         <>
-        <p>Count: {count}</p>
-        <button onClick={countClick}>{title}</button>
+        <h1>{names[index]}</h1>
+        <button onClick={()=>{
+            setName(index+1)
+        }}>Show Next</button>
         </>
     )
 }
